3c50436f117d10f63db472650e94deb4c632d644
Lyonsaldanha <saldanhalyon@yahoo.in>
2026-02-17 16:05:43 +0530

    added new styles and improved flow

--- blog/views.py
+++ blog/views.py
@@ -13,12 +13,20 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView): 
     model = Post
     template_name = "post_detail.html"
-
+    
 class BlogCreateView(CreateView):  # new
     model = Post
     template_name = "post_new.html"
-    fields = ["title", "author", "body"]
+    fields = ["title", "body"]  # Removed 'author' - will be set automatically
+    success_url = reverse_lazy("home")
+    
+    def form_valid(self, form):
+        # Automatically set the author to the logged-in user
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
+
+    
 class BlogUpdateView(UpdateView):
     model = Post
     template_name = "post_edit.html"
